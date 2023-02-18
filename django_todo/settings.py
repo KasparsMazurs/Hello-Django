3cfe61adf1f0ee8ed12d04443ed7de89a398b7e3
KasparsMazurs <kaspars.mazurs@gmail.com>
2023-02-18 14:37:10 +0000

    fix some bugs

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9s!&tl16ciiq8@nrt=(^=0s0=t40f&($v&m5(szznp7_15xtwi'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-9s!&tl16ciiq8@nrt=(^=0s0=t40f&($v&m5(szznp7_15xtwi')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #     }
 # }
 
-DATABASES = { 'default': dj_database_url.parse (os.environ.get('DATABASE_URL')) }
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
